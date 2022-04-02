149cf6e31a6f97858c171a05a05b40eea6547bdc
Bruno Totoli <santosbruno101062@gmail.com>
2022-04-01 23:52:59 -0300

    Treeset compara elementos

---
+++ src/Aula08/program/Application.java
@@ -0,0 +1,20 @@
+package Aula08.program;
+
+import Aula08.entities.Product;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Application {
+    public static void main(String[] args) {
+        Set<Product> set = new TreeSet<>();
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+        for (Product p : set) {
+            System.out.println(p);
+        }
+    }
+}
+
+
